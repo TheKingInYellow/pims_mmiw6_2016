1dcc097b42da2df59930636e0da07c5f84c4ab8b
TheKingInYellow <kody.crowell1598@gmail.com>
2016-08-09 16:28:01 -0700

    removed annoying prompts and commented plotting / np.cumsum

--- traffic_2way.py
+++ traffic_2way.py
@@ -1,10 +1,6 @@
 
 # coding: utf-8
 
-# 
-
-# In[1]:
-
 # import libraries
 import matplotlib
 from __future__ import division, print_function
@@ -12,10 +8,7 @@ import numpy as np
 import scipy.integrate as spi
 import matplotlib.pyplot as plt
 
-get_ipython().magic('matplotlib inline')
-
-
-# In[2]:
+# get_ipython().magic('matplotlib inline')
 
 # define all time related constants
 TIME_G_1 = 20 # seconds
@@ -26,7 +19,7 @@ T = TIME_G_1 + TIME_R_1 # seconds
 dt = 1
 
 # define arrival constraints
-LAMBDA_1 = 20 
+LAMBDA_1 = 20
 LAMBDA_2 = 25
 
 # define departure rate
@@ -36,22 +29,13 @@ RHO_2 = 26
 # define initial conditions
 q0, t0 = [0, 0], 0
 
-
-# In[3]:
-
 # arrivals dependent on poisson process
 ARRIVAL_1 = np.random.poisson(lam=LAMBDA_1)
 ARRIVAL_2 = np.random.poisson(lam=LAMBDA_2)
 
-
-# In[4]:
-
 # define indicator functions
 def I1(t): return 1.0 if np.mod(t,T) <= TIME_G_1 else 0.0
 def I2(t): return 1.0 if np.mod(t,T) >= TIME_G_1 else 0.0
-
-
-# In[5]:
 
 # define our right hand side
 def f(t,lam,rho,ind):
@@ -59,30 +43,18 @@ def f(t,lam,rho,ind):
 
 def RHS(t): return [f(t,LAMBDA_1,RHO_1,I1(t)), f(t,LAMBDA_2,RHO_2,I2(t))]
 
-
-# In[6]:
-
-queue_t = []
-for t in np.arange(0,T,dt):
-    queue_t.append(RHS(t))
-fcn = np.reshape(queue_t,[T*dt,2])
-fcn
-
-
-# In[14]:
-
-sol=np.floor(np.cumsum(fcn,axis=0)*dt)
-sol
-
-
-# In[9]:
+# OBSOLETE : solution using numpy's cumsum
+## queue_t = []
+## for t in np.arange(0,T,dt):
+##     queue_t.append(RHS(t))
+## fcn = np.reshape(queue_t,[T*dt,2])
+## sol=np.floor(np.cumsum(fcn,axis=0)*dt)
+# fcn
+# sol
 
 # set up scipy ode integrator
 q=spi.ode(RHS).set_integrator("vode", method="bdf")
 q.set_initial_value(q0, t0)
-
-
-# In[10]:
 
 # iterate to get results
 time = []
@@ -94,28 +66,17 @@ while q.successful() and q.t < T:
     queues.append(resp)
     print(step, resp)
 
-
-# In[17]:
-
 # array manipulation
 time = np.asarray(time)
 queues = np.squeeze(queues)
 
-
-# In[32]:
-
 # generate pretty plots
-fig = plt.plot(time,queues[:,0], 'b-', )
-plt.plot(time, queues[:,1], 'r--')
-ax = plt.gca()
-ax.set_title("Queue Size vs. Time")
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Queue Size (# Cars)")
-ax.grid()
-plt.show()
-
-
-# In[ ]:
-
-
+## fig = plt.plot(time,queues[:,0], 'b-', )
+## plt.plot(time, queues[:,1], 'r--')
+## ax = plt.gca()
+## ax.set_title("Queue Size vs. Time")
+## ax.set_xlabel("Time (s)")
+## ax.set_ylabel("Queue Size (# Cars)")
+## ax.grid()
+## plt.show()
 
