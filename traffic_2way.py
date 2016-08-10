cca5acae811763dc365e4523d90a8791e3bff609
TheKingInYellow <kody.crowell1598@gmail.com>
2016-08-10 09:50:09 -0700

    random fixes

--- traffic_2way.py
+++ traffic_2way.py
@@ -3,7 +3,6 @@
 
 # import libraries
 import matplotlib
-from __future__ import division, print_function
 import numpy as np
 import scipy.integrate as spi
 import matplotlib.pyplot as plt
@@ -11,64 +10,64 @@ import matplotlib.pyplot as plt
 # get_ipython().magic('matplotlib inline')
 
 # define all time related constants
-TIME_G_1 = 20 # seconds
-TIME_R_1 = 20 # seconds
-TIME_G_2 = 20 # seconds
-TIME_R_2 = 20 # seconds
-T = TIME_G_1 + TIME_R_1 # seconds
-dt = 1
+# TIME_G_1 = 20 # seconds
+# TIME_R_1 = 20 # seconds
+# TIME_G_2 = 20 # seconds
+# TIME_R_2 = 20 # seconds
+# T = TIME_G_1 + TIME_R_1 # seconds
+# dt = 1
 
 # define arrival constraints
-LAMBDA_1 = 20
-LAMBDA_2 = 25
+# LAMBDA_1 = 20
+# LAMBDA_2 = 25
 
 # define departure rate
-RHO_1 = 19
-RHO_2 = 26
+# RHO_1 = 19
+# RHO_2 = 26
 
 # define initial conditions
-q0, t0 = [0, 0], 0
+# q0, t0 = [0, 0], 0
 
 # arrivals dependent on poisson process
-ARRIVAL_1 = np.random.poisson(lam=LAMBDA_1)
-ARRIVAL_2 = np.random.poisson(lam=LAMBDA_2)
+# ARRIVAL_1 = np.random.poisson(lam=LAMBDA_1)
+# ARRIVAL_2 = np.random.poisson(lam=LAMBDA_2)
 
 # define indicator functions
-def I1(t): return 1.0 if np.mod(t,T) <= TIME_G_1 else 0.0
-def I2(t): return 1.0 if np.mod(t,T) >= TIME_G_1 else 0.0
+# def I1(t): return 1.0 if np.mod(t,T) <= TIME_G_1 else 0.0
+# def I2(t): return 1.0 if np.mod(t,T) >= TIME_G_1 else 0.0
 
 # define our right hand side
-def f(t,lam,rho,ind):
-    return lam - rho*ind
+# def f(t,lam,rho,ind):
+#     return lam - rho*ind
 
-def RHS(t): return [f(t,LAMBDA_1,RHO_1,I1(t)), f(t,LAMBDA_2,RHO_2,I2(t))]
+# def RHS(t): return [f(t,LAMBDA_1,RHO_1,I1(t)), f(t,LAMBDA_2,RHO_2,I2(t))]
 
 # OBSOLETE : solution using numpy's cumsum
-## queue_t = []
-## for t in np.arange(0,T,dt):
-##     queue_t.append(RHS(t))
-## fcn = np.reshape(queue_t,[T*dt,2])
-## sol=np.floor(np.cumsum(fcn,axis=0)*dt)
+# queue_t = []
+# for t in np.arange(0,T,dt):
+#     queue_t.append(RHS(t))
+# fcn = np.reshape(queue_t,[T*dt,2])
+# sol=np.floor(np.cumsum(fcn,axis=0)*dt)
 # fcn
 # sol
 
 # set up scipy ode integrator
-q=spi.ode(RHS).set_integrator("vode", method="bdf")
-q.set_initial_value(q0, t0)
+# q=spi.ode(RHS).set_integrator("vode", method="bdf")
+# q.set_initial_value(q0, t0)
 
 # iterate to get results
-time = []
-queues = []
-while q.successful() and q.t < T:
-    step = q.t+dt
-    resp = q.integrate(q.t+dt)
-    time.append(step)
-    queues.append(resp)
-    print(step, resp)
+# time = []
+# queues = []
+# while q.successful() and q.t < T:
+#     step = q.t+dt
+#     resp = q.integrate(q.t+dt)
+#     time.append(step)
+#     queues.append(resp)
+#     print(step, resp)
 
 # array manipulation
-time = np.asarray(time)
-queues = np.squeeze(queues)
+# time = np.asarray(time)
+# queues = np.squeeze(queues)
 
 # generate pretty plots
 ## fig = plt.plot(time,queues[:,0], 'b-', )
@@ -85,10 +84,10 @@ queues = np.squeeze(queues)
 
 def main(lamb):
     # define all time related constants
-    TIME_G_1 = 20 # seconds
-    TIME_R_1 = 20 # seconds
-    TIME_G_2 = 20 # seconds
-    TIME_R_2 = 20 # seconds
+    TIME_G_1 = 60 # seconds
+    TIME_R_1 = 60 # seconds
+    TIME_G_2 = 60 # seconds
+    TIME_R_2 = 60 # seconds
     T = TIME_G_1 + TIME_R_1 # seconds
     dt = 1
 
@@ -97,8 +96,8 @@ def main(lamb):
     LAMBDA_2 = lamb[1]
 
     # define departure rate
-    RHO_1 = 19
-    RHO_2 = 26
+    RHO_1 = 1.0
+    RHO_2 = 1.2
 
     # define initial conditions
     q0, t0 = [10, 10], 0
@@ -117,15 +116,6 @@ def main(lamb):
 
     def RHS(t): return [f(t,LAMBDA_1,RHO_1,I1(t)), f(t,LAMBDA_2,RHO_2,I2(t))]
 
-    # OBSOLETE : solution using numpy's cumsum
-    ## queue_t = []
-    ## for t in np.arange(0,T,dt):
-    ##     queue_t.append(RHS(t))
-    ## fcn = np.reshape(queue_t,[T*dt,2])
-    ## sol=np.floor(np.cumsum(fcn,axis=0)*dt)
-    # fcn
-    # sol
-
     # set up scipy ode integrator
     q=spi.ode(RHS).set_integrator("vode", method="bdf")
     q.set_initial_value(q0, t0)
@@ -137,21 +127,21 @@ def main(lamb):
         step = q.t+dt
         resp = q.integrate(q.t+dt)
         resp[resp<0]=0
-#        resp = max([resp,0])
         time.append(step)
         queues.append(resp)
-    #    print(step, resp)
+    #   print(step, resp)
 
     # array manipulation
     time = np.asarray(time)
     queues = np.squeeze(queues)
 
     # generate pretty plots
-    fig = plt.plot(time,queues[:,0], 'b-', )
-    plt.plot(time, queues[:,1], 'r--')
+    fig = plt.plot(time,queues[:,0], 'b-', label="Street 1")
+    plt.plot(time, queues[:,1], 'r--', label="Street 2")
     ax = plt.gca()
     ax.set_title("Queue Size vs. Time")
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Queue Size (# Cars)")
     ax.grid()
+    ax.legend(loc='best')
     plt.show()
