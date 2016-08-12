63443251ab2d0f816265a71cc5cee778590ff6fa
Philippe Gaudreau <pjgaudre@ualberta.ca>
2016-08-11 20:47:44 -0600

    with Optimization

--- traffic_2way.py
+++ traffic_2way.py
@@ -2,32 +2,44 @@
 # coding: utf-8
 
 # import libraries
+# import libraries
+from __future__ import division, print_function
 import matplotlib
 import numpy as np
 import scipy.integrate as spi
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
-from matplotlib.colors import ListedColormap, BoundaryNorm
+from matplotlib.colors import ListedColormap, BoundaryNorm, Normalize
+from scipy.optimize import minimize_scalar
 
+def tau(q0,rho,lamb,T,TIME_G):
+    if TIME_G > -q0/(lamb-rho):
+        qbar =  0.5*( q0**2 / (rho-lamb)  + lamb *(T-TIME_G)**2)/T
+    else:
+        qbar =  0.5*( rho*(T-TIME_G)**2 - (rho-lamb)*T**2)/T + q0
+    return (qbar + 1)/(rho*TIME_G)
+
+# get_ipython().magic('matplotlib inline')
 def main(lamb,i):
     # define all time related constants
-    TIME_G_1 = 20 # seconds
-    TIME_R_1 = 40 # seconds
+    T = 60
+#    TIME_G_1 = 30.9265046289334506 # seconds
+#    TIME_R_1 = T-TIME_G_1 # seconds
 #    TIME_G_2 = TIME_R_1 # seconds
 #    TIME_R_2 = TIME_G_1 # seconds
-    T = TIME_G_1 + TIME_R_1 # seconds
+#    T = TIME_G_1 + TIME_R_1 # seconds
     dt = .01    #time step in cumsum method
-
     # define arrival constraints
     LAMBDA_1 = lamb[0]
     LAMBDA_2 = lamb[1]
     # define departure rate
-    RHO_1 = 2.0
-    RHO_2 = 3.0
-
+    RHO_1 = 10.0
+    RHO_2 = 10.0
     # define initial conditions
-    q0, t0 = [7, 20], 0
+    q0, t0 = [20, 10], 0
 
+    def WAWT(t,initial):
+       return (LAMBDA_1*tau(initial[0],RHO_1,LAMBDA_1,T,t) + LAMBDA_2*tau(initial[1],RHO_2,LAMBDA_2,T,T-t))/(LAMBDA_1+LAMBDA_2)
     # arrivals dependent on poisson process
  #   ARRIVAL_1 = np.random.poisson(lam=LAMBDA_1)
  #   ARRIVAL_2 = np.random.poisson(lam=LAMBDA_2)
@@ -51,8 +63,13 @@ def main(lamb,i):
         return sol
     # OBSOLETE : solution using numpy's cumsum
     # First part of the solution: time interval (t0,TIME_G_1)
+    initial = q0
+    c = 5.5
+    GreenLightTime = []
+#    bound = np.sort([abs(-initial[0]/(LAMBDA_1-RHO_1)),T-abs(initial[1]/(LAMBDA_2-RHO_2)) ])
+    TIME_G_1 = minimize_scalar(WAWT, args=(initial), bounds=(0+c, T-c), method='bounded').x
+    GreenLightTime.append(TIME_G_1)
     time = np.asarray(np.arange(t0,TIME_G_1,dt))
-    Slope = RHS(time[1])
     sol = Solution(time,q0)
     # Lets try two cycles:
     numcycles = 3
@@ -62,14 +79,16 @@ def main(lamb,i):
         start = np.mod(index,2)*TIME_G_1 + np.floor(index/2)*T
         end = np.mod(index+1,2)*TIME_G_1 + np.floor((index+1)/2)*T
         time_temp =  np.asarray(np.arange(start,end,dt))
-        Slope.append(RHS(time_temp[1]))
         sol_temp = Solution(time_temp,initial)
-        initial = sol_temp[-1,]
         time = np.concatenate((time,time_temp))
         sol = np.concatenate((sol,sol_temp))
-
+        initial = sol_temp[-1,]
+        if np.mod(j,2)==0:
+            GreenLightTime.append(T-TIME_G_1)
+        else:
+            GreenLightTime.append(TIME_G_1)
+        TIME_G_1 =  minimize_scalar(WAWT, args=(initial), bounds=(0+c, T-c), method='bounded').x if np.mod(j,2)==0 else TIME_G_1
     queues = np.squeeze(sol)
-    print(Slope)
 #    # set up scipy ode integrator
 #    q=spi.ode(RHS).set_integrator("vode", method="bdf")
 #    q.set_initial_value(q0, t0)
@@ -87,6 +106,7 @@ def main(lamb,i):
 #    #    print(step, resp)
 
     # generate pretty plots
+    # generate pretty plots
     fig = plt.plot(time,queues[:,0], 'k-', label="Street 1", alpha=0.8)
     plt.plot(time, queues[:,1], 'k--', label="Street 2", alpha=0.8)
     ax = plt.gca()
@@ -102,15 +122,15 @@ def main(lamb,i):
 
     #fig = plt.figure()
     #ax = fig.add_subplot(111)
+    GreenLightTime = np.squeeze(GreenLightTime)
     ax.set_title("Queue Size vs. Time")
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Queue Size (# Cars)")
-
     points1 = np.array([time, queues[:,0]]).T.reshape(-1,1,2)
     segments1 = np.concatenate([points1[:-1], points1[1:]], axis=1)
     cmap1 = ListedColormap(['g','r'])
     lc1 = LineCollection(segments1, cmap=cmap1, \
-            norm=BoundaryNorm([TIME_G_1], cmap1.N), \
+            norm=BoundaryNorm(GreenLightTime, cmap1.N), \
             linewidth = 3, linestyle = 'dashed')
     lc1.set_array(np.mod(time,T))
     #lc1.set_linestyle('dashed')
@@ -121,7 +141,7 @@ def main(lamb,i):
     segments2 = np.concatenate([points2[:-1], points2[1:]], axis=1)
     cmap2 = ListedColormap(['r','g'])
     lc2 = LineCollection(segments2, cmap=cmap2, \
-            norm=BoundaryNorm([TIME_G_1], cmap2.N), \
+            norm=BoundaryNorm(GreenLightTime, cmap2.N), \
             linewidth = 3, linestyle = 'solid')
     lc2.set_array(np.mod(time,T))
     lc2.set_linewidth(3)
@@ -133,7 +153,8 @@ def main(lamb,i):
     plt.close()
 
 
-lamb= [[1.4321,2.654],[3.231,2.12],[1.76,4.4325],[3.9876,4.5432],[2.213,3.5356],[0.323,0.3322]]
+lamb= [[1.4321,2.654],[3.231,2.12],[1.76,4.4325],[3.9876,4.5432],[2.213,3.5356]]
+#
 L = len(lamb)
 for i in range(len(lamb)):
     main(lamb[i],i)
